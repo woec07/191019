bc0bbf1e625acb2ed6f3ed167e08dbd19cbf6241
jjones <jefferson.jones@tuwien.ac.at>
2025-10-30 15:15:34 +0100

    Fix verilator paths

--- sources/misc/scripts/build_verilator.sh
+++ sources/misc/scripts/build_verilator.sh
@@ -4,14 +4,16 @@ if [ ! -d /opt/hwe ]; then
     echo "opt/hwe directory doesnt exist.  Creating it"
     mkdir /opt/hwe
 fi
-cd /opt/hwe
+cd /opt/hwe/
 INSTALL_PATH=$PWD/verilator
 
 #Download
-git clone https://github.com/verilator/verilator
+sudo git clone https://github.com/verilator/verilator verilator_source
 unset VERILATOR_ROOT
-cd verilator
-git checkout tags/v5.030
-autoconf
+cd verilator_source
+sudo git checkout tags/v5.030
+sudo autoconf
 sudo ./configure --prefix $INSTALL_PATH
 sudo make -j8
+sudo make install 
+sudo cp -r  /opt/hwe/verilator_source/include/ /opt/hwe/verilator/
