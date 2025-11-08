4b75e0d3470645613751752b2d189d283d2be3b8
Matthias WOECKINGER (ACA participant) <s12023400@ti29.tilab.tuwien.ac.at>
2025-11-08 17:32:54 +0100

    matmul started, not working

--- student_files/testcases/matmul/matmul.cpp
+++ student_files/testcases/matmul/matmul.cpp
@@ -1,5 +1,5 @@
 #include "matmul.hpp"
-
+#include "riscv_vector.h"
 void matmul(
   int8_t * pSrcA,
   int8_t * pSrcB,
@@ -9,75 +9,39 @@ void matmul(
   uint32_t numColsB)
   
 {
-        int32_t sum;                                     /* Accumulator */
-        int8_t *pIn1 = pSrcA;                    /* Input data matrix pointer A */
-        int8_t *pIn2 = pSrcB;                    /* Input data matrix pointer B */
-        int8_t *pInA = pSrcA;                    /* Input data matrix pointer A of Q15 type */
-        int8_t *pInB = pSrcB;                    /* Input data matrix pointer B of Q15 type */
-        int32_t *pOut = pDst;                     /* Output data matrix pointer */
-        int32_t *px;                                     /* Temporary output data matrix pointer */
-        uint32_t col, i = 0U, row = numRowsA, colCnt;  /* Loop counters */
+  // not working yet
+  // TODO: how to integrate vl into the algorithm??????????
 
-  {
-    /* The following loop performs the dot-product of each row in pSrcA with each column in pSrcB */
-    /* row loop */
-    do
-    {
-      /* Output pointer is set to starting address of the row being processed */
-      px = pOut + i;
-
-      /* For every row wise process, column loop counter is to be initiated */
-      col = numColsB;
-
-      /* For every row wise process, pIn2 pointer is set to starting address of pSrcB data */
-      pIn2 = pSrcB;
-
-      /* column loop */
-      do
-      {
-        /* Set the variable sum, that acts as accumulator, to zero */
-        sum = 0;
-
-        /* Initiate pointer pIn1 to point to starting address of pSrcA */
-        pIn1 = pInA;
-
-        /* Matrix A columns number of MAC operations are to be performed */
-        colCnt = numColsA;
-
-        /* matrix multiplication */
-        while (colCnt > 0U)
-        {
-          /* c(m,n) = a(1,1) * b(1,1) + a(1,2) * b(2,1) + .... + a(m,p) * b(p,n) */
-
-          /* Perform multiply-accumulates */
-          sum += (int32_t) * pIn1++ * *pIn2;
-          pIn2 += numColsB;
-
-          /* Decrement loop counter */
-          colCnt--;
-        }
-        /* Store result in destination buffer */
-        *px++ = sum;
-
-        /* Decrement column loop counter */
-        col--;
-
-        /* Update pointer pIn2 to point to starting address of next column */
-        pIn2 = pInB + (numColsB - col);
-
-      } while (col > 0U);
-
-      /* Update pointer pSrcA to point to starting address of next row */
-      i = i + numColsB;
-      pInA = pInA + numColsA;
-
-      /* Decrement row loop counter */
-      row--;
-
-    } while (row > 0U);
+  uint32_t numRowsB = numColsA;
+  uint32_t j = 0;
+  // take row after row from A
+  while (j < numRowsA){
+    uint32_t i = 0;
+    vint32m4_t rowRes, helper;
+    while (i < numRowsB){
+      // takes the i-th row of B (1,2,3, ... numRowsB)
+      vint8m1_t rowB_i = __riscv_vle8_v_i8m1(pSrcB, numColsB);
+      // takes the i_th element of row j of A
+      int8_t a_i =  *pSrcA;
+      vint16m2_t mulScal = __riscv_vwmul_vx_i16m2(rowB_i, a_i, numColsB);
+      vint32m4_t mulScal_ex = __riscv_vsext_vf2_i32m4(mulScal, numColsB);
+      if (i == 0) {
+        helper = mulScal_ex;
+      } 
+      else{
+        rowRes = __riscv_vadd_vv_i32m4(helper, mulScal_ex, numColsB);
+        helper = rowRes;
+      }
+            
+      pSrcA++;
+      pSrcB += numColsB;
+      i++;
+    }
+    // store first result row to destination
+    __riscv_vse32_v_i32m4(pDst, rowRes, numColsB);
+    pDst += numColsB;
+    j++;
   }
-
   /* Return to application */
   return;
 }
-
