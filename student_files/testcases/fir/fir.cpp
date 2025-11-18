cf1569b0072fc749173cf7da5c9126517c62f7c7
Matthias WOECKINGER (ACA participant) <s12023400@ti29.tilab.tuwien.ac.at>
2025-11-18 12:22:06 +0100

    fir solved, still pissed

--- student_files/testcases/fir/fir.cpp
+++ student_files/testcases/fir/fir.cpp
@@ -12,42 +12,36 @@ void fir(
   /* Calculate the length of the output per channel based on inputs */
   uint32_t output_len = input_len - (filter_len - 1);
 
-  /* Channel Loop Counter */
-  uint32_t ch = 0;
-  while (ch < filter_channels)
-  {
-    /* Set input, filter and output pointers*/
-    int32_t * out_ptr = output + ch*output_len;
-    int16_t * in_ptr = input;
-    int16_t * in_ptr_base = input;
+  int16_t * currInput = input;
+  volatile int16_t * currFilter = filter;
+  int32_t * currOutput = output;
 
-    /* Output Loop Counter */
-    uint32_t out = 0;
-    while (out < output_len)
-    {
-      /* Result Accumulator */
-      int32_t acc = 0;
-      /* Set in_ptr to start of next iteration of convolution and filter_ptr to start of filter */
-      in_ptr = in_ptr_base;
-      int16_t * filter_ptr = filter + ch*filter_len;
-      /* Filter Loop Counter */
-      uint32_t fil = 0;
-      while (fil < filter_len)
+  uint32_t i = 0;
+  while (i < filter_channels){
+    currInput = input;
+    currOutput = output + i * output_len; // i think i got this wrong in the exam (we need to use output_len to overwrite the incorrectly written!!!!)
+    currFilter = filter + i * filter_len;
+    uint32_t remInput = input_len;
+    while (remInput > 0U)
       {
-        /* Multiply Element of the filter with the corresponding element of the input */
-        acc += (int32_t)*(in_ptr++) * (int32_t)*(filter_ptr++);
-
-        /* Increment filter loop counter*/
-        fil++;
+        size_t vl = __riscv_vsetvl_e32m8(remInput);
+        vint32m8_t result = __riscv_vmv_v_x_i32m8(0,vl);
+        for (int j = 0; j < filter_len; j++){
+          vl = __riscv_vsetvl_e16m4(remInput);
+          vint16m4_t rowA = __riscv_vle16_v_i16m4(currInput, vl);
+          int16_t filElm = *currFilter;
+          result = __riscv_vwmacc_vx_i32m8(result,filElm,rowA, vl);
+          currInput++;
+          currFilter++;
+        }
+        // during exam i tried with these pointers, so i got there some errors in the final version
+        __riscv_vse32_v_i32m8(currOutput, result, vl); // crucial: this vl is okay (but in the first i thought this, but i kept searching for the error so it got wild)
+        currFilter = filter + i * filter_len; // obvious
+        currOutput += vl; //obvious
+        currInput = currInput - filter_len + vl; // this is a bit tricky, but just basically increment currInput by vl in each iteration (compared to the beginning)
+        remInput -= vl; // obvious
       }
-      /* Store result and advance pointers */
-      *out_ptr++ = acc;
-      in_ptr_base++;
-      /* Increment output loop counter*/
-      out++;
-    }
-    /* Increment channel loop counter */
-    ch++;
+    i++;
   }
   return;
 }
